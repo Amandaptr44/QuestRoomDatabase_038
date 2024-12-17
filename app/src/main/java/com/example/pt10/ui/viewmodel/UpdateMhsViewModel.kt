1a47a28c72bcc4d54af1b45d81ea779f12737d9c
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-12-17 13:17:57 +0700

    Mengambil data siswa berdasarkan NIM dari pegangan status yang disimpan, dan petakan ke `MhsUIState`

---
+++ app/src/main/java/com/example/pt10/ui/viewmodel/UpdateMhsViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.pt10.ui.viewmodel
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pt10.data.entity.Mahasiswa
+import com.example.pt10.repository.RepositoryMhs
+import com.example.pt10.ui.navigation.DestinasiUpdate
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class UpdateMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
+    var updateUIState by mutableStateOf(MhsUIState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+
+    init {
+        viewModelScope.launch {
+            updateUIState = repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUIStateMhs()
+        }
+    }
+
