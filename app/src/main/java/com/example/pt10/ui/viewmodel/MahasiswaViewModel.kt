38a8cf481fb8e21bb21c6e301078ad66a5141c6d
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-12-17 13:08:24 +0700

    FormErrorState untuk menyertakan metode `isValid()` untuk validasi data siswa yang lebih efisien

--- app/src/main/java/com/example/pt10/ui/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/pt10/ui/viewmodel/MahasiswaViewModel.kt
@@ -76,3 +76,21 @@ data class MhsUIState(
     val snackbarMessage: String? = null,
 )
 
+data class FormErrorState(
+    val nim : String? = null,
+    val nama : String? = null,
+    val jenisKelamin : String? = null,
+    val alamat : String? = null,
+    val kelas : String? = null,
+    val angkatan: String? = null
+){
+    fun isValid(): Boolean{
+        return nim == null
+                && nama == null
+                && jenisKelamin == null
+                && alamat == null
+                && kelas == null
+                && angkatan == null
+    }
+}
+
