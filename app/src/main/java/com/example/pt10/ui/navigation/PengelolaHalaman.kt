39b9901a7172d46364775edd495f0db6ad2371a5
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-12-17 12:08:53 +0700

    Refactor PengelolaHalaman untuk menggunakan DestinasiHome.route dari AlamatNavigasi.

--- app/src/main/java/com/example/pt10/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pt10/ui/navigation/PengelolaHalaman.kt
@@ -3,11 +3,16 @@ package com.example.pt10.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.praktikumpertemuan10.ui.view.mahasiswa.DestinasiInsert
 import com.example.praktikumpertemuan10.ui.view.mahasiswa.InserMhsView
+import com.example.pt10.ui.view.mahasiswa.DetailMhsView
+import com.example.pt10.ui.view.mahasiswa.HomeMhsView
+import com.example.pt10.ui.view.mahasiswa.UpdateMhsView
 
 @Composable
 fun PengelolaHalaman(
@@ -16,13 +21,4 @@ fun PengelolaHalaman(
 ){
     NavHost(
         navController = navController,
-        startDestination = DestinasiInsert.route) {
-        composable(
-            route = DestinasiInsert.route
-        ){
-            InserMhsView(
-                  onBack = { }, onNavigate = { }
-            )
-        }
-    }
-}
+        startDestination = DestinasiHome.route) {
